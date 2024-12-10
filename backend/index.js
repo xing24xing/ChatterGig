385490e578386cd7baf9eef80818370dccc6faf9
xing24xing <khushipandey260304@gmail.com>
2024-12-10 19:23:03 +0530

    deploy

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.URL,
     credentials: true
 }));
 app.use(cookieParser());
--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,6 +1,6 @@
-export const USER_API = "http://localhost:8080/api/v1/user"
-export const MSG_API = "http://localhost:8080/api/v1/message"
-export const BASE_URL = "http://localhost:8080"
+export const USER_API = "https://chattergig.onrender.com/api/v1/user"
+export const MSG_API = "https://chattergig.onrender.com/pi/v1/message"
+export const BASE_URL = "https://chattergig.onrender.com"
 // utils/playNotificationSound.js
 export const playNotificationSound = () => {
     const audio = new Audio('/ding.mp3');
